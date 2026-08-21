d8df371cffe5138e4773f7028a075637515daaaa
residencia270med <medaprovacaomedicina@gmail.com>
2026-08-21 01:27:34 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "residencia270-v1";
+const CACHE = "residencia270-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
