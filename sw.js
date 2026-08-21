8b13f132fdf7ec871c277cc5bacc2fda2d3ae836
residencia270med <medaprovacaomedicina@gmail.com>
2026-08-21 01:41:50 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ self.addEventListener("install", event => {
   self.skipWaiting();
 });
 self.addEventListener("activate", event => {
+event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));  
   event.waitUntil(self.clients.claim());
 });
 self.addEventListener("fetch", event => {
